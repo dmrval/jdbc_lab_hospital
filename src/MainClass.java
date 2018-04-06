172e3859e129392b38c7bb5e30e8bbc1c276f2db
DamirValeev <dmr.val@mail.ru>
2018-04-06 23:29:07 +0400

    6.04(3)

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,21 +4,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class MainClass {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//        Class.forName("org.sqlite.JDBC");
-//        try (Connection connentTo = DriverManager.getConnection("jdbc:sqlite:MusicSalone.db")) {
-//        }
-//        System.out.println("Главное меню: ");
-//        int token;
-//        Scanner scanner = new Scanner(System.in);
-//        token = scanner.nextInt();
-//        switch (token) {
-//            case 1:
-//            case 2:
-//            case 3:
-//            case 4:
-//                default:
-//        }
-        GiveAllMenu.giveMainMenu();
+    public static void main(String[] args) {
+
     }
 }
